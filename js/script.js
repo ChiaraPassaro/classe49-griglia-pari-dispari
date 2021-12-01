543062b4fa6dd2cd2f18aac034337a255232a782
Chiara Passaro <chiarapassaro.wd@gmail.com>
2021-12-01 08:35:46 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Creare in JavaScript una griglia 8x8.
+// Ogni cella della griglia contiene un numero casuale da 1 a 64. 
+
+//I numeri presenti nelle celle non devono essere ripetuti(ovvero la griglia contiene ** tutti ** i numeri da 1 a 64).
+
+// Ogni volta che clicco su un quadrato questo si colora di verde se il numero contenuto e' pari e in rosso se dispari..
